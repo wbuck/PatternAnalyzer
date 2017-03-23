cd2ee3957ae169f0dc48678725de7b14f0617ca1
Warren Buckley <wbuckley15@hotmail.com>
2017-03-23 13:37:26 -0400

    Add find edge and average edge. Add units tests

--- PatternAnalyzer/PatternAnalyzer/Analyzer.cs
+++ PatternAnalyzer/PatternAnalyzer/Analyzer.cs
@@ -4,15 +4,13 @@ using System.Drawing;
 using System.Linq;
 using Emgu.CV;
 using Emgu.CV.Cvb;
-using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
-using PatternAnalyzer.Structures;
 
 namespace PatternAnalyzer
 {
     public class Analyzer
     {
-        public IEnumerable<Blob> FindBlobs( Bitmap src, uint minArea, uint maxArea )
+        public CvBlobs FindBlobs( Bitmap src, uint minArea, uint maxArea )
         {
             if( src == null )
             {
@@ -21,8 +19,8 @@ namespace PatternAnalyzer
 
             using( var sourceImage = new Image<Gray, byte>( src ) )
             using( var blobDetector = new CvBlobDetector( ) )
-            using( var blobs = new CvBlobs( ) )
             {
+                var blobs = new CvBlobs( );
                 Image<Gray, byte> filteredSrc = null;
 
                 try
@@ -39,7 +37,7 @@ namespace PatternAnalyzer
                     // have an average area of roughly 3500 pixels.
                     blobs.FilterByArea( ( int )minArea, ( int )maxArea );
 
-                    return blobs.Values.Select( b => new Blob( b.Centroid, b.Area, b.BoundingBox, b.GetContour( ) ) );
+                    return blobs;
                 }
                 finally
                 {
@@ -47,7 +45,77 @@ namespace PatternAnalyzer
                 }
             }
         }
+
+        public LineSegment2D FindEdge( Bitmap src )
+        {
+            if( src == null )
+            {
+                throw new ArgumentNullException( nameof( src ), @"Source image cannot be null" );
+            }
+
+            using( var sourceImage = new Image<Gray, byte>( src ) )
+            {
+                var edge = default( LineSegment2D );
+                Image<Gray, byte> filteredSrc = null;
+
+                try
+                {
+                    // Filter the source image. First I down sample
+                    // the image to smooth out any noise. I then
+                    // apply canny to the image (essentially binarizing)
+                    // the image before I apply I dilate filter to help
+                    // accentuate any contours.
+                    filteredSrc = sourceImage.PyrDown( )
+                        .PyrUp( )
+                        .Canny( 100, 200 )
+                        .Dilate( 3 );
+
+                    // Find all lines in the filtered image.
+                    // Note that the treshold parameter is 
+                    // actually refering to the number of 
+                    // required points (length) for a line.
+                    var edges = filteredSrc.HoughLinesBinary( rhoResolution : 1,
+                        thetaResolution : Math.PI / 180,
+                        threshold : 500,
+                        minLineWidth : 30,
+                        gapBetweenLines : 50 )[ 0 ];
+
+                    if( edges.Length > 0 )
+                    {
+                        // Find the edge with the longest length
+                        // from the list of found edges.
+                        edge = edges.Aggregate( ( edg1, edg2 ) => edg1.Length > edg2.Length ? edg1 : edg2 );
+                    }
+                    else
+                    {
+                        // No edges were found so return a
+                        // default edge structure.
+                        return edge;
+                    }
+                }
+                finally
+                {
+                    filteredSrc?.Dispose( );
+                }
+                return edge;
+            }
+        }
+
+        public PointF FindAveragePoint( IEnumerable<PointF> points )
+        {
+            if( points == null )
+            {
+                throw new ArgumentNullException( nameof( points ), "Collection cannot be null" );
+            }
+
+            var pointFs = points as IList<PointF> ?? points.ToList( );
+
+            if( !pointFs.Any( ) )
+            {
+                return default( PointF );
+            }
+
+            return new PointF( pointFs.Average( p => p.X ), pointFs.Average( p => p.Y ) );
+        }
     }
-
-
 }
---
+++ PatternAnalyzer/PatternAnalyzerTests/AnalyzerTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Emgu.CV.Structure;
+using PatternAnalyzer;
+using PatternAnalyzerTests.Properties;
+using Xunit;
+
+namespace PatternAnalyzerTests
+{
+    public class AnalyzerTests
+    {
+        [Fact]
+        public void FindBlobsTestWithTwentyBlobs( )
+        {
+            var image = ( Bitmap )Resources.ResourceManager.GetObject( "GoodImageTwentyDots" );
+            const uint minArea = 2000;
+            const uint maxArea = 5000;
+            const int expectedBlobsFound = 20;
+
+            var analyzer = new Analyzer( );
+
+            var result = analyzer.FindBlobs( image, minArea, maxArea );
+            Assert.Equal( expectedBlobsFound, result.Count );
+        }
+
+        [Fact]
+        public void FindBlobsTestWithNoBlobs( )
+        {
+            var image = ( Bitmap )Resources.ResourceManager.GetObject( "BadImageNoDots" );
+            const uint minArea = 2000;
+            const uint maxArea = 5000;
+            const int expectedBlobsFound = 0;
+
+            var analyzer = new Analyzer( );
+
+            var result = analyzer.FindBlobs( image, minArea, maxArea );
+            Assert.Equal( expectedBlobsFound, result.Count );
+        }
+
+        [Fact]
+        public void FindBlobsTestWithNull( )
+        {
+            Bitmap image = null;
+            const uint minArea = 2000;
+            const uint maxArea = 5000;
+
+            var analyzer = new Analyzer( );
+
+            var exception = Record.Exception( ( ) => {
+                analyzer.FindBlobs( image, minArea, maxArea );
+            } );
+
+            Assert.NotNull( exception );
+            Assert.IsType<ArgumentNullException>( exception );
+
+        }
+
+        [Fact]
+        public void FindEdgeTestWithEdge( )
+        {
+            var image = ( Bitmap )Resources.ResourceManager.GetObject( "GoodImageTwentyDots" );
+            var analyzer = new Analyzer( );
+
+            var result = analyzer.FindEdge( image );
+
+            Assert.NotEqual( default( LineSegment2D ), result );
+        }
+
+        [Fact]
+        public void FindEdgeTestWithNoEdge( )
+        {
+            var image = ( Bitmap )Resources.ResourceManager.GetObject( "BadImageNoEdge" );
+            var analyzer = new Analyzer( );
+
+            var result = analyzer.FindEdge( image );
+
+            Assert.Equal( default( LineSegment2D ), result );
+        }
+
+        [Fact]
+        public void FindEdgeTestWithNull( )
+        {
+            Bitmap image = null;
+            var analyzer = new Analyzer( );
+
+            var exception = Record.Exception( ( ) => {
+                analyzer.FindEdge( image );
+            } );
+
+            Assert.NotNull( exception );
+            Assert.IsType<ArgumentNullException>( exception );
+        }
+
+        [Fact]
+        public void FindAveragePointTestWithFivePoints( )
+        {
+            var testPoints = new List<PointF>
+            {
+                new PointF( 10, 50 ),
+                new PointF( 20, 40 ),
+                new PointF( 30, 30 ),
+                new PointF( 40, 20 ),
+                new PointF( 50, 10 )
+            };
+            
+            var expectedPoint = new PointF( 30, 30 );
+
+            var analyzer = new Analyzer( );
+            var result = analyzer.FindAveragePoint( testPoints );
+
+            Assert.Equal( expectedPoint, result );
+        }
+
+        [Fact]
+        public void FindAveragePointTestWithNull( )
+        {
+            List<PointF> testPoints = null;
+            var analyzer = new Analyzer( );
+
+            var exception = Record.Exception( ( ) => {
+                analyzer.FindAveragePoint( testPoints );
+            } );
+
+            Assert.NotNull( exception );
+            Assert.IsType<ArgumentNullException>( exception );
+        }
+    }
+}

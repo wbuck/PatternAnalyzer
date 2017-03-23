44d822dafeae65b7a4e15317e51dd3fb1bc7a5eb
Warren Buckley <wbuckley15@hotmail.com>
2017-03-23 08:51:52 -0400

    Add blob object and blob detection

--- PatternAnalyzer/PatternAnalyzer/Analyzer.cs
+++ PatternAnalyzer/PatternAnalyzer/Analyzer.cs
@@ -1,77 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using Emgu.CV;
+using Emgu.CV.Cvb;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using PatternAnalyzer.Structures;
 
 namespace PatternAnalyzer
 {
     public class Analyzer
     {
-        private Mat BinarizeImage( IInputArray frame, double threshold, double maxValue, ThresholdType thresholdType )
+        public IEnumerable<Blob> FindBlobs( Bitmap src, uint minArea, uint maxArea )
         {
-            var binaryImage = new Mat( );
-            
-            // Binarize the image martix.         
-            CvInvoke.Threshold( frame, binaryImage, threshold, maxValue, thresholdType );
-            return binaryImage;
-        }
-
-        private void FindTopPaperEdge( IImage frame, out LineSegment2DF paperEdge, out float paperSlope )
-        {
-            // Threshold and invert the image in order
-            // to detect the edge of the paper.
-            using( var image = BinarizeImage( frame, 90, 255, ThresholdType.BinaryInv ).ToImage<Gray, byte>( ) )
+            if( src == null )
             {
-                // Detect the edge(s) of the paper.
-                var lines = image.PyrDown( )
-                    .PyrUp( )
-                    .Canny( 200, 255 )
-                    .Dilate( 3 )
-                    .HoughLinesBinary( rhoResolution : 1,
-                        thetaResolution : Math.PI / 45.0,
-                        threshold : 50,
-                        minLineWidth : 100,
-                        gapBetweenLines : 1 )[ 0 ];
-
-                // The HoughLinesBinary method was returning multiple lines
-                // which represented the edge of the paper. This was causing 
-                // an issue when trying to determine the equation and slope
-                // of the line. To correct this the left most Point and the
-                // right most Point were taken in order to create only 1 line
-                // in which the line equation and slope could be derived from.
-                using( var img = new Image<Bgr, byte>( frame.Size ) )
-                {
-                    // Find the smallest X coordinate.
-                    var min = lines.Min( line => line.P1.X );
-
-                    // Find the largest X coordinate.
-                    var max = lines.Max( line => line.P2.X );
-
-                    // Find the Point which equals the smallest
-                    // X coordinate found above.
-                    var startPoint = lines.FirstOrDefault( line => line.P1.X == min ).P1;
-
-                    // Find the Point which equals the largest
-                    // X coordinate found above.
-                    var endPoint = lines.FirstOrDefault( line => line.P2.X == max ).P2;
-
-                    // Create a new line segment from the Point from above
-                    // which will represent the edge of the paper.
-                    paperEdge = new LineSegment2DF( startPoint, endPoint );
-
-                    // Determine the slope (if any) of the line representing
-                    // the paper edge.
-                    paperSlope = ( ( paperEdge.P2.Y - paperEdge.P1.Y ) / ( paperEdge.P2.X - paperEdge.P1.X ) );
-
-                    // Draw the paper edge line on the image for
-                    // presentation purposes only.
-                    img.Draw( paperEdge, new Bgr( Color.Red ), 8 );
-//                    img.Or( threshold.ToImage<Bgr, byte>( ) ).ToBitmap( ).Save( "" );
-                }
+                throw new ArgumentNullException( nameof( src ), @"Source image cannot be null" );
             }
 
+            using( var sourceImage = new Image<Gray, byte>( src ) )
+            using( var blobDetector = new CvBlobDetector( ) )
+            using( var blobs = new CvBlobs( ) )
+            {
+                Image<Gray, byte> filteredSrc = null;
+
+                try
+                {
+                    // Binarize and invert the image so that
+                    // that blob detector can locate the dots.
+                    filteredSrc = sourceImage.ThresholdBinaryInv( new Gray( 90 ), new Gray( 255 ) );
+
+                    // Finds all blobs in the input image and
+                    // stores them in to the CvBlobs structure.
+                    blobDetector.Detect( filteredSrc, blobs );
+
+                    // Filter the blobs by area. The alignment dots
+                    // have an average area of roughly 3500 pixels.
+                    blobs.FilterByArea( ( int )minArea, ( int )maxArea );
+
+                    return blobs.Values.Select( b => new Blob( b.Centroid, b.Area, b.BoundingBox, b.GetContour( ) ) );
+                }
+                finally
+                {
+                    filteredSrc?.Dispose( );
+                }
+            }
         }
     }
+
+
 }
